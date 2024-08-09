877b0a5d2a492a52e350d022f2218141b43aa918
vovastelmashchuk <vovochkastelmashchuk@gmail.com>
2024-08-09 17:57:58 +0300

    commit

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,5 @@
 local wezterm = require 'wezterm';
 
-
 return {
   color_scheme = 'Dark Ocean (terminal.sexy)',
   font = wezterm.font("0xProto Nerd Font"),
@@ -11,6 +10,31 @@ return {
       key = 't',
       mods = 'CMD',
       action = wezterm.action { SpawnTab = "CurrentPaneDomain" }
+    },
+    {
+      key = 'w',
+      mods = 'CMD',
+      action = wezterm.action { CloseCurrentPane = { confirm = true } }
+    },
+    {
+      key = "\\",
+      mods = "CMD",
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = "-",
+      mods = "CMD",
+      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+      key = "c",
+      mods = "CMD",
+      action = wezterm.action.CopyTo 'Clipboard'
+    },
+    {
+      key = "v",
+      mods = "CMD",
+      action = wezterm.action.PasteFrom 'Clipboard'
     },
     {
       key = "1",
@@ -38,14 +62,14 @@ return {
       action = wezterm.action { ActivateTab = 4 }
     },
     {
+      key = "6",
+      mods = "CMD",
+      action = wezterm.action { ActivateTab = 5 }
+    },
+    {
       key = "p",
       mods = "CMD",
       action = wezterm.action { PaneSelect = { alphabet = "123456789" } }
-    },
-    {
-      key = "\\",
-      mods = "CMD",
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
     }
   }
 }
